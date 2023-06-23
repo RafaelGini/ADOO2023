8d6c9b9846278acdcdf0a3aa16ab42d3e8d150f7
Vicky <31855542+Fatike23@users.noreply.github.com>
2023-06-23 20:22:07 -0300

    Add files via upload

---
+++ test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+
+}
+
+/*
+-Cargar nuevos socios.
+-Cargar ejemplares (libros, revistas especializadas, revistas, diarios).
+-Buscar ejemplares según el criterio solicitado y mostrar datos completos (incluyendo ubicación).
+-Actualizar el estado de ejemplares prestados presencialmente o vía web (préstamo y devolución).
+-Actualizar parámetros de préstamos (plazo en días).
+-Notificar a socios acerca de situaciones particulares (próximo vencimiento, penalización por devolución fuera de término, premio por devoluciones).
+-Visualizar el historial de préstamos de un socio.
+*/
